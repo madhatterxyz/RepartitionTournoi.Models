bf4066400d1363020722d74d327339eb5a591df6
Nicolas <Nicolas@PC-NICOLAS>
2022-08-08 22:03:23 +0200

    models dto

--- CompositionDTO.cs
+++ CompositionDTO.cs
@@ -2,7 +2,13 @@
 {
     public class CompositionDTO
     {
-        public IEnumerable<MatchDTO> Matchs { get; set; }
+        public List<MatchDTO> Matchs { get; set; }
         public JeuDTO Jeu { get; set; }
+        public MatchDTO Match { get; set; }
+        public TournoiDTO Tournoi { get; set; }
+
+        public long MatchId { get; set; }
+        public long JeuId { get; set; }
+        public long TournoiId { get; set; }
     }
 }
---
+++ TounoirDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace RepartitionTournoi.Models
+{
+    public class TournoiDTO
+    {
+        public TournoiDTO()
+        {
+
+        }
+        public TournoiDTO(long id, string nom)
+        {
+            Id = id;
+            Nom = nom;
+            Compositions = new List<CompositionDTO>();
+        }
+    
+        public long Id { get; set; }
+        public string Nom { get; set; }
+        public List<CompositionDTO> Compositions { get; set; }
+
+    }
+}
